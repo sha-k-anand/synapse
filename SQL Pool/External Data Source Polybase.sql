ed3e46a55490401660030a93f0f01332a35a09fc
SKA <sha.anand@outlook.com>
2022-08-15 16:43:07 -0500

    Added

--- SQL Pool/External Data Source Polybase.sql
+++ SQL Pool/External Data Source Polybase.sql
@@ -20,3 +20,34 @@ SELECT * FROM [external_DimProductSubcategory1]
 --Double quoted
 CREATE EXTERNAL FILE FORMAT [TextFileFormat_001] WITH (FORMAT_TYPE = DELIMITEDTEXT, FORMAT_OPTIONS (FIELD_TERMINATOR = N',',STRING_DELIMITER='0x22', FIRST_ROW = 2,USE_TYPE_DEFAULT=TRUE))
 
+
+-------------------------------------
+
+CREATE DATABASE SCOPED CREDENTIAL msi_cred  WITH IDENTITY = 'Managed Service Identity'
+CREATE EXTERNAL DATA SOURCE [ext_datasource1]  WITH (TYPE = HADOOP, LOCATION = N'abfss://cont01@zzsynapsembnettest005.dfs.core.windows.net',CREDENTIAL = msi_cred)
+CREATE EXTERNAL FILE FORMAT [TextFileFormat_HiveCtrlA1] WITH (FORMAT_TYPE = DELIMITEDTEXT, FORMAT_OPTIONS (FIELD_TERMINATOR = N'0x01', DATE_FORMAT = N'yyyy-MM-dd HH:mm:ss', FIRST_ROW = 2, USE_TYPE_DEFAULT = False))
+
+CREATE EXTERNAL TABLE [dbo].[external_DimProductSubcategory2]
+(
+	[ProductSubcategoryKey] [int],
+	[ProductSubcategoryAlternateKey] [int] ,
+	[EnglishProductSubcategoryName] [nvarchar](50) ,
+	[SpanishProductSubcategoryName] [nvarchar](50) ,
+	[FrenchProductSubcategoryName] [nvarchar](50) ,
+	[ProductCategoryKey] [int] 
+)
+WITH (DATA_SOURCE = [ext_datasource1],LOCATION = N'/DimProductSubcategory.txt',FILE_FORMAT = [TextFileFormat_HiveCtrlA1],REJECT_TYPE = VALUE,REJECT_VALUE = 0)
+
+SELECT * FROM [dbo].[external_DimProductSubcategory2]
+
+--
+DROP EXTERNAL TABLE [dbo].[external_DimProductSubcategory2]
+GO
+DROP EXTERNAL FILE FORMAT [TextFileFormat_HiveCtrlA1]
+GO
+DROP EXTERNAL DATA SOURCE [ext_datasource1] 
+GO
+DROP DATABASE SCOPED CREDENTIAL msi_cred 
+
+
+select * from sys.credentials
